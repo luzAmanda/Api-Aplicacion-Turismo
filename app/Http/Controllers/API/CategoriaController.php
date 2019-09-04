03dba6a9299942c3e79868b4357b096e2796512e
luzAmanda <luz_magcq94@hotmail.com>
2019-09-04 13:39:50 -0500

    Api categoria

--- app/Http/Controllers/API/CategoriaController.php
+++ app/Http/Controllers/API/CategoriaController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
+use App\Categoria;
 
-class CategoriaController extends Controller
+class CategoriaController extends BaseController
 {
     
     public function index()
     {
   $categorias=  Categoria::with('detalle','detallesSucursales')->get();
+
+    return $this->sendResponse($categorias->toArray(), 'Categorias retrieved successfully.');
+    }
+
+    public function show($id)
+    {
+  $categorias=  Categoria::find($id);
 
     return $this->sendResponse($categorias->toArray(), 'Categorias retrieved successfully.');
     }
